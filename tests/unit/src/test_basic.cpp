b441db556b2e0331619924b977387428a8a85e2c
AkhilTThomas <akhilthomasmec@gmail.com>
2025-03-15 22:31:45 +0100

    Add unit test for grpc failures

--- tests/unit/src/test_basic.cpp
+++ tests/unit/src/test_basic.cpp
@@ -46,6 +46,24 @@ TEST(KuksaClientTest, getValue) {
   EXPECT_EQ(value.float_(), 12.23f);
 }
 
+TEST(KuksaClientTest, getValue_failed) {
+  auto mock_stub = std::make_unique<kuksa::val::v2::MockVALStub>();
+
+  EXPECT_CALL(*mock_stub, GetValue)
+      .WillOnce([](grpc::ClientContext *ctx,
+                   const kuksa::val::v2::GetValueRequest &request,
+                   kuksa::val::v2::GetValueResponse *response) -> grpc::Status {
+        EXPECT_EQ(request.signal_id().path(), "Vehicle.Speed");
+        return grpc::Status::CANCELLED;
+      });
+
+  kuksa::KuksaClient client(std::move(mock_stub));
+  kuksa::val::v2::Value value{};
+  bool status = client.getValue("Vehicle.Speed", value);
+
+  EXPECT_FALSE(status);
+}
+
 TEST(KuksaClientTest, publish_value) {
   auto mock_stub = std::make_unique<kuksa::val::v2::MockVALStub>();
 
@@ -67,6 +85,27 @@ TEST(KuksaClientTest, publish_value) {
   EXPECT_TRUE(status);
 }
 
+TEST(KuksaClientTest, publish_value_failed) {
+  auto mock_stub = std::make_unique<kuksa::val::v2::MockVALStub>();
+
+  EXPECT_CALL(*mock_stub, PublishValue)
+      .WillOnce(
+          [](grpc::ClientContext *ctx,
+             const kuksa::val::v2::PublishValueRequest &request,
+             kuksa::val::v2::PublishValueResponse *response) -> grpc::Status {
+            EXPECT_EQ(request.signal_id().path(), "Vehicle.Speed");
+            EXPECT_EQ(request.data_point().value().float_(), 52.23f);
+            return grpc::Status::CANCELLED;
+          });
+
+  kuksa::KuksaClient client(std::move(mock_stub));
+  kuksa::val::v2::Value value{};
+  value.set_float_(52.23f);
+  bool status = client.publishValue("Vehicle.Speed", value);
+
+  EXPECT_FALSE(status);
+}
+
 TEST(KuksaClientTest, actuate) {
   auto mock_stub = std::make_unique<kuksa::val::v2::MockVALStub>();
 
@@ -85,6 +124,26 @@ TEST(KuksaClientTest, actuate) {
   bool status = client.actuate("Vehicle.Body.Trunk.Rear.IsOpen", value);
 
   EXPECT_TRUE(status);
+}
+
+TEST(KuksaClientTest, actuate_failed) {
+  auto mock_stub = std::make_unique<kuksa::val::v2::MockVALStub>();
+
+  EXPECT_CALL(*mock_stub, Actuate)
+      .WillOnce([](grpc::ClientContext *ctx,
+                   const kuksa::val::v2::ActuateRequest &request,
+                   kuksa::val::v2::ActuateResponse *response) -> grpc::Status {
+        EXPECT_EQ(request.signal_id().path(), "Vehicle.Body.Trunk.Rear.IsOpen");
+        EXPECT_EQ(request.value().bool_(), true);
+        return grpc::Status::CANCELLED;
+      });
+
+  kuksa::KuksaClient client(std::move(mock_stub));
+  kuksa::val::v2::Value value{};
+  value.set_bool_(true);
+  bool status = client.actuate("Vehicle.Body.Trunk.Rear.IsOpen", value);
+
+  EXPECT_FALSE(status);
 }
 
 TEST(KuksaClientTest, getServerInfo) {
@@ -110,6 +169,88 @@ TEST(KuksaClientTest, getServerInfo) {
   EXPECT_EQ(serverInfo.name(), "kuksa_gtest_mock");
   EXPECT_EQ(serverInfo.version(), "1.2.3");
   EXPECT_EQ(serverInfo.commit_hash(), "123456789abcdefg");
+}
+
+TEST(KuksaClientTest, getServerInfo_failed) {
+  auto mock_stub = std::make_unique<kuksa::val::v2::MockVALStub>();
+
+  EXPECT_CALL(*mock_stub, GetServerInfo)
+      .WillOnce(
+          [](grpc::ClientContext *ctx,
+             const kuksa::val::v2::GetServerInfoRequest &request,
+             kuksa::val::v2::GetServerInfoResponse *response) -> grpc::Status {
+            *response->mutable_name() = "kuksa_gtest_mock";
+            *response->mutable_version() = "1.2.3";
+            *response->mutable_commit_hash() = "123456789abcdefg";
+            return grpc::Status::CANCELLED;
+          });
+
+  kuksa::KuksaClient client(std::move(mock_stub));
+
+  kuksa::val::v2::GetServerInfoResponse serverInfo{};
+  bool status = client.getServerInfo(serverInfo);
+
+  EXPECT_FALSE(status);
+}
+
+TEST(KuksaClientTest, getValues) {
+  auto mock_stub = std::make_unique<kuksa::val::v2::MockVALStub>();
+
+  EXPECT_CALL(*mock_stub, GetValues)
+      .WillOnce(
+          [](grpc::ClientContext *ctx,
+             const kuksa::val::v2::GetValuesRequest &request,
+             kuksa::val::v2::GetValuesResponse *response) -> grpc::Status {
+            EXPECT_EQ(request.signal_ids(0).path(), "Vehicle.Speed");
+            EXPECT_EQ(request.signal_ids(1).path(),
+                      "Vehicle.Chassis.Accelerator.PedalPosition");
+
+            auto dp1 = response->add_data_points();
+            dp1->mutable_value()->set_float_(42.35f);
+
+            auto dp2 = response->add_data_points();
+            dp2->mutable_value()->set_uint32(67);
+
+            return grpc::Status::OK;
+          });
+
+  kuksa::KuksaClient client(std::move(mock_stub));
+
+  std::vector<kuksa::val::v2::Datapoint> datapoints;
+  std::vector<std::string> signals = {
+      "Vehicle.Speed", "Vehicle.Chassis.Accelerator.PedalPosition"};
+
+  datapoints = client.getValues(signals);
+
+  EXPECT_EQ(datapoints.at(0).value().float_(), 42.35f);
+  EXPECT_EQ(datapoints.at(1).value().uint32(), 67);
+}
+
+TEST(KuksaClientTest, getValues_failed) {
+  auto mock_stub = std::make_unique<kuksa::val::v2::MockVALStub>();
+
+  EXPECT_CALL(*mock_stub, GetValues)
+      .WillOnce(
+          [](grpc::ClientContext *ctx,
+             const kuksa::val::v2::GetValuesRequest &request,
+             kuksa::val::v2::GetValuesResponse *response) -> grpc::Status {
+            EXPECT_EQ(request.signal_ids(0).path(), "Vehicle.Speed");
+            EXPECT_EQ(request.signal_ids(1).path(),
+                      "Vehicle.Chassis.Accelerator.PedalPosition");
+
+            return grpc::Status::CANCELLED;
+          });
+
+  kuksa::KuksaClient client(std::move(mock_stub));
+
+  std::vector<kuksa::val::v2::Datapoint> datapoints;
+  std::vector<std::string> signals = {
+      "Vehicle.Speed", "Vehicle.Chassis.Accelerator.PedalPosition"};
+
+  datapoints = client.getValues(signals);
+
+  // expect empty vector
+  EXPECT_EQ(0, datapoints.size());
 }
 
 int main(int argc, char **argv) {
